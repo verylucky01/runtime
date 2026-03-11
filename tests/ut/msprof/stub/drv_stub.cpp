5005a303819d9f5d82ae3963f0185dba2a3f6394
z296249221 <zhengkai40@huawei.com>
2026-03-11 17:51:17 +0800

    [Profiling]恢复avp模块编译打包，修复gcc10版本ut用例编译失败问题
    Co-authored-by: z00622916<zhengkai40@huawei.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !753 merge profiling-avp-build into master
    
    [Profiling]恢复avp模块编译打包，修复gcc10版本ut用例编译失败问题
    
    Created-by: z296249221
    Commit-by: z00622916
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    1. 恢复avp模块编译打包
    2. 修复gcc10版本ut用例编译失败问题
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    Issue [#196](https://gitcode.com/cann/runtime/issues/196) 
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    编译通过即可
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    NA
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [x] 其他，请描述：
    已有功能恢复。
    
    
    
    
    See merge request: cann/runtime!753

--- tests/ut/msprof/stub/drv_stub.cpp
+++ tests/ut/msprof/stub/drv_stub.cpp
@@ -8,6 +8,7 @@
  * See LICENSE in the root of the software repository for the full text of the License.
  */
 #include <map>
+#include <string>
 #include <cstring>
 #include "ascend_hal.h"
 #include "mmpa_api.h"

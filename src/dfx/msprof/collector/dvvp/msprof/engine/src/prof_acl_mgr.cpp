061c5ae6078b84169f8a22818281b9f9472143ad
Seanesmhxocism <617225691@qq.com>
2026-03-24 19:57:02 +0800

    [PROFILING][9.0.0]revert prof flush
    Co-authored-by: Seanesmhxocism<617225691@qq.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1125 merge cherry-pick-mr-1123-1774264093799-auto into 9.0.0
    
    [PROFILING][9.0.0]revert prof flush
    
    Created-by: Seanesmhxocism
    Commit-by: Seanesmhxocism
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    prof 在warmup调用flush接口引入较多问题。多次修改后依然有遗留问题。
    当前新增qos数据异常问题，疑似flush接口调用引入（回退后不复现）。目前无法定位解决，暂时将flush接口回退规避类似问题。
    
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    重跑相关冒烟用例，问题不复现
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/runtime!1125

--- src/dfx/msprof/collector/dvvp/msprof/engine/src/prof_acl_mgr.cpp
+++ src/dfx/msprof/collector/dvvp/msprof/engine/src/prof_acl_mgr.cpp
@@ -2945,8 +2945,6 @@ void ProfAclMgr::ChangeProfWarmupToStart(const std::vector<uint32_t> &devIds) co
     for (auto &devId : devIds) {
         ProfAclMgr::instance()->DumpStartInfoFile(devId);
     }
-    // flush drv data
-    ProfChannelManager::instance()->FlushChannel();
     UploaderMgr::instance()->SetUploadDataIfStart(true);
 }
 }   // namespace Api

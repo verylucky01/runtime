1e25f836cdf3edc1fa0ae585c88372f19636030b
zhangxinxing_xx <zhangxingxing33@hisilicon.com>
2026-04-21 11:46:21 +0800

    fix bug
    Co-authored-by: zhangxinxing_xx<zhangxingxing33@hisilicon.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !1693 merge 4211 into master
    
    fix bug
    
    Created-by: zhangxinxing_xx
    Commit-by: zhangxinxing_xx
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    RevisedWait流程中event可为nullptr 需要特殊处理
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。例如：关联Issue #000-->
    https://gitcode.com/cann/runtime/issues/385
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    上板用例验证
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    不涉及
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [x] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/runtime!1693

--- src/runtime/core/src/task/task_info/event_task.cc
+++ src/runtime/core/src/task/task_info/event_task.cc
@@ -282,7 +282,6 @@ void ConstructSqeForEventRecordTask(TaskInfo * const taskInfo, rtStarsSqe_t *con
 rtError_t EventResetTaskInit(TaskInfo * const taskInfo, Event *const eventPtr,
                              const bool isNotifyFlag, const int32_t eventIndex)
 {
-    NULL_PTR_RETURN(eventPtr, RT_ERROR_INVALID_VALUE);
     EventResetTaskInfo *eventResetTaskInfo = &(taskInfo->u.eventResetTaskInfo);
     TaskCommonInfoInit(taskInfo);
 
@@ -292,9 +291,11 @@ rtError_t EventResetTaskInit(TaskInfo * const taskInfo, Event *const eventPtr,
     eventResetTaskInfo->isNotify = isNotifyFlag;
     eventResetTaskInfo->eventid = eventIndex;
     eventResetTaskInfo->event = eventPtr;
-    taskInfo->taskOwner =
-        (eventPtr->EventOwner_() == EventOwner::EVENT_INNER) ?
-        static_cast<uint8_t>(TaskOwner::RT_TASK_INNER) : taskInfo->taskOwner;
+    if (eventPtr != nullptr) {
+        taskInfo->taskOwner =
+            (eventPtr->EventOwner_() == EventOwner::EVENT_INNER) ?
+            static_cast<uint8_t>(TaskOwner::RT_TASK_INNER) : taskInfo->taskOwner;
+    }
     return RT_ERROR_NONE;
 }
 
@@ -487,7 +488,6 @@ void DoCompleteSuccessForRemoteEventWaitTask(TaskInfo * const taskInfo, const ui
 rtError_t EventWaitTaskInit(TaskInfo * const taskInfo, Event *const eventRec, const int32_t eventIndex,
                             const uint32_t timeout, const uint8_t waitFlag)
 {
-    NULL_PTR_RETURN(eventRec, RT_ERROR_INVALID_VALUE);
     EventWaitTaskInfo *eventWaitTaskInfo = &(taskInfo->u.eventWaitTaskInfo);
     TaskCommonInfoInit(taskInfo);
 
@@ -497,9 +497,11 @@ rtError_t EventWaitTaskInit(TaskInfo * const taskInfo, Event *const eventRec, co
     eventWaitTaskInfo->eventId = eventIndex;
     eventWaitTaskInfo->timeout = timeout;
     eventWaitTaskInfo->eventWaitFlag = waitFlag;
-    taskInfo->taskOwner =
-        (eventRec->EventOwner_() == EventOwner::EVENT_INNER) ?
-        static_cast<uint8_t>(TaskOwner::RT_TASK_INNER) : taskInfo->taskOwner;
+    if (eventRec != nullptr) {
+        taskInfo->taskOwner =
+            (eventRec->EventOwner_() == EventOwner::EVENT_INNER) ?
+            static_cast<uint8_t>(TaskOwner::RT_TASK_INNER) : taskInfo->taskOwner;
+    }
 
     return RT_ERROR_NONE;
 }
